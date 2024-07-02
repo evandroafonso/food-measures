d75c15dddc76b1992b1c2db2939d3115496552be
Evandro Afonso Rodrigues <evandroafonsorodrigues@gmail.com>
2024-07-01 22:02:22 -0300

    add exception handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (Exception $e) {
+            return response()->json([
+                'message' => 'Acces do not alowed. Do login and try again!',
+                'code' => 401
+            ]);
+        });
+        // https://www.itsolutionstuff.com/post/exception-error-handling-in-laravel-11example.html
     })->create();
